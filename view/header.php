77ac0187f8b6012a4a43e5c82c3dc4113be09394
Julien Vendran <julien.vendran@etu-umontpellier.fr>
2019-11-22 13:48:40 +0100

    View et Header

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<div id="bandeau">
+    <img src="bandeau.png" alt="logo">
+</div>
+<nav>
+    <div id="menu" class="nav-wrapper">
+        <ul id="nav-mobile" class="hide-on-med-and-down">
+            <li><a href="index.php?readAllVins">Vins</a></li>
+            <li><a href="index.php?readAllViticulteurs">Viticulteurs</a></li>
+            <li><a href="index.php?readAllDomaines">Domaines</a></li>
+        </ul>
+    </div>
+</nav>
--- view/view.php
+++ view/view.php
@@ -1,34 +1,22 @@
 <!DOCTYPE html>
 <html>
     <head>
-            <!-- Compiled and minified CSS -->
+        <!-- Compiled and minified CSS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <!-- Compiled and minified JavaScript -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link href="./view/style.css" type="text/css" rel="stylesheet">
+        <link type="text/css" href="./view/style.css" rel="stylesheet">
         <meta charset="UTF-8">
             <title>Vente de vins</title>
     </head>
     <body>
             <header>
-                    <div id="bandeau">
-                            <img src="./view/images/bandeau.png" alt="logo">
-                    </div>
-                    <nav>
-                        <div id="menu" class="nav-wrapper">
-                          <ul id="nav-mobile" class="hide-on-med-and-down">
-                            <li><a href="badges.html">Actualité</a></li>
-                            <li><a href="sass.html">Acceuil</a></li>
-                            <li><a href="collapsible.html">Boutique</a></li>
-                          </ul>
-                        </div>
-                    </nav>
+                   <?php require_once File::build_path(array('view', 'header.php'));?>
             </header>
             <main>
                 <?php
-                $filepath = File::build_path(array("view", $controller, "$view.php"));
-                require $filepath;
+                require File::build_path(array("view", $controller, "$view.php"));
                 ?>
             </main>
             <footer>
