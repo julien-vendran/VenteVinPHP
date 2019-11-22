2f7ebe612921251ebb527db415fcd053549a9dba
Julien Vendran <julien.vendran@etu-umontpellier.fr>
2019-11-22 15:53:23 +0100

    Ajout utilisateur

--- view/header.php
+++ view/header.php
@@ -7,6 +7,12 @@
             <li><a href="?action=readAllVins">Boutique</a></li>
             <li><a href="index.php">Acceuil</a></li>
             <li><a href="?action=readAllDomaines">Domaines</a></li>
+            <?php
+                if (!Session::est_Connecte())
+                    echo '<li><a href="?action=connectUser">Se Connecter</a></li>';
+                else
+                    echo '<li><a href="?action=deconnectUser">Se Deconnecter</a></li>';
+            ?>
         </ul>
     </div>
 </nav>
