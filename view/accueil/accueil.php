5fefb09e82f1f4b28504ff6d666060e47fa7ab54
Nicolas BOFI <nicolas.bofi@gmail.com>
2019-11-22 15:30:41 +0100

    Mise en page de l'accueil (forme globale)

--- view/accueil/accueil.php
+++ view/accueil/accueil.php
@@ -1,6 +1,33 @@
 <div class="parallax-container">
-    <div class="parallax"><img src="images/cave.jpg"></div>
+    <div class="parallax"><img src="./view/images/cave.png"></div>
+</div>
+<div class="container">
+    <h1>Bienvenue sur caveau-online.fr</h1>
     <p>
-        <h1> Bienvenue </h1>
+        Trouver un bon vin peut parfois être difficle. <br>
+        Grâce à notre boutique trouver facilement les meilleures bouteilles et millesime, tout sa livrer directement chez vous et garantit sans casse.
     </p>
 </div>
+<div class="parallax-container">
+    <div class="parallax"><img src="./view/images/cave.png"></div>
+</div>
+
+<div class="container">
+    Retrouver notre large selection de vin directment dans notre boutique.
+
+</div>
+
+<div class="container">
+    Découvrer nos domaines partenaires, leur histoire ainsi que les vins qu'ils produisent directement sur leur page.
+
+</div>
+
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script> //Script effet parallax (image mouvante début de page)
+    $(document).ready(function(){
+        $('.parallax').parallax();
+    });
+</script>
+
