5930034a476426011cbd1b4ffb949e5e708275f9
Nicolas BOFI <nicolas.bofi@gmail.com>
2019-11-22 14:12:10 +0100

    Midification du nav

--- view/header.php
+++ view/header.php
@@ -4,9 +4,9 @@
 <nav>
     <div id="menu" class="nav-wrapper">
         <ul id="nav-mobile" class="hide-on-med-and-down">
-            <li><a href="badges.html">Actualité</a></li>
-            <li><a href="sass.html">Acceuil</a></li>
-            <li><a href="collapsible.html">Boutique</a></li>
+            <li><a href="?action=readAllVins">Boutique</a></li>
+            <li><a href="index.php">Acceuil</a></li>
+            <li><a href="?action=readAllDomaines">Domaines</a></li>
         </ul>
     </div>
 </nav>
