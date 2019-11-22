f08e8934dbebfc159338ad64eef0ff43c2fe4517
Julien Vendran <julien.vendran@etu-umontpellier.fr>
2019-11-22 13:50:16 +0100

    View et Header

--- view/view.php
+++ view/view.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <!-- Compiled and minified CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-        <!-- Compiled and minified JavaScript -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link type="text/css" href="./view/style.css" rel="stylesheet">
-        <meta charset="UTF-8">
-            <title>Vente de vins</title>
-    </head>
-    <body>
-            <header>
-                   <?php require_once File::build_path(array('view', 'header.php'));?>
-            </header>
-            <main>
-                <?php
-                require File::build_path(array("view", $controller, "$view.php"));
-                ?>
-            </main>
-            <footer>
-                    <div>
-                    © 2019 Copyright caveau-online.fr 
-                    </div>
-            </footer>
-    </body>
+<head>
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="./view/style.css" type="text/css" rel="stylesheet">
+    <meta charset="UTF-8">
+    <title>Vente de vins</title>
+</head>
+<body>
+<header>
+    <div id="bandeau">
+        <?php
+        require File::build_path(array("view", $controller, "$view.php"));
+        ?>
+        <main>
+            <?php
+            $filepath = File::build_path(array("view", $controller, "$view.php"));
+            require $filepath;
+            ?>
+        </main>
+        <footer>
+            <div>
+                © 2019 Copyright caveau-online.fr
+            </div>
+        </footer>
+</body>
 </html>
