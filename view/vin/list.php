bd67df02a297997b1e001ee59efc1cd03f87814e
Julien Vendran <julien.vendran@etu-umontpellier.fr>
2019-11-29 16:00:23 +0100

    Avancement affichage vin

--- view/vin/list.php
+++ view/vin/list.php
@@ -1,26 +1,30 @@
-<?php
-echo '<ul>';?>
 <div class="row">
 <?php
     foreach ($tab as $v) {
         $nomVin = $v->get('nomVin');
         $prixVin = $v->get('prixVin');
-        $descrVin = $v->get('descriptionVin');
+        $prixVin = $v->get('prixVin');
         $imgVin = $v->get('imageVin');
         ?>
-            <div class="col s4 m2">
-                    <li> <!-- c dent une boucle-->
-                        <div class="card">
-                            <div class="card-image">
+            <div class="col s4 m2"> <!-- c dent une boucle-->
+                        <div class="card sizecard">
+                            <div class="card-image sizeimg">
                                 <img src="<?php echo $imgVin?>">
-                                <a class="btn-floating btn-large halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
                             </div>
-                            <div class="card-content">
+                            <div class="card-content ">
                                 <span class="black-text card-title"><?php echo $nomVin?></span>
-                                <p class = "black-text"><?php echo $descrVin?></p>
+                                <p class = "black-text"> <?php echo $prixVin ?>€ / bouteille</p>
+                                <form action = "index.php">
+                                    <input type="number" step="1" min="0" max="" name="quantity" value="1" title="Qté" size="4" pattern="[0-9]*" inputmode="numeric">
+                                    <button class="btn-floating btn-large halfway-fab waves-effect waves-light red" type="submit">
+                                        <i class="material-icons">
+                                            add_shopping_cart
+                                        </i>
+                                    </button>
+
+                                </form>
                             </div>
                         </div>
-                </li>
             </div>
 
         <?php
@@ -30,3 +34,5 @@ echo '<ul>';?>
 </div>
 <?php
     echo "<a id=\"create\" href = \"?action=createVin\">Créer une autre Vin</a>";
+
+
