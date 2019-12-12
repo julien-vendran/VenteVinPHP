2ec31308137457ea616e7bc1d9f13ba05900f478
Mathis-Goichon <mathis.goichon@etu.umontpellier.fr>
2019-12-12 22:22:04 +0100

    Amélioration affichage

--- view/header.php
+++ view/header.php
@@ -16,14 +16,13 @@
                     echo '<li><a href="?action=connectUser">Connexion</a></li>';
                 else {
                     echo '<li><a href="?action=deconnectUser">Deconnexion</a></li>';
+                    if (Session::est_viticuleur())
+                        echo '<li><a href="?action=adminPanelViticulteur">Viticulteur</a></li>';
+
+                    if (Session::est_administrateur())
+                        echo "<li><a href='?action=adminPanelAdministrateur'>Admin</a></li>";
                     echo '<li><a href="?action=detailUser"><i class="material-icons">account_circle</i></a>';
                 }
-
-                if (Session::est_viticuleur())
-                    echo '<li><a href="?action=adminPanelViticulteur">Panneau viticulteur</a></li>';
-
-                if (Session::est_administrateur())
-                    echo "<li><a href='?action=adminPanelAdministrateur'>Panneau administrateur</a></li>";
             ?>
         </ul>
     </div>
